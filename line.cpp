3af32bd63c516ad141d373e11035f372996d2ef6
Devon Loehr <devon.loehr@gmail.com>
2016-01-17 17:46:18 -0500

    Started work on a class for oriented lines

---
+++ line.cpp
@@ -0,0 +1,129 @@
+#include "line.h"
+#include <cmath>
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846264338328
+#endif // M_PI
+#include <iostream>
+
+Line::Line(sf::RenderWindow* window, line_type type, sf::Vector2f start, sf::Vector2f finish):
+    m_window(window),
+    m_type(type),
+    m_start(start),
+    m_finish(finish),
+    m_body(sf::Points,0)
+{
+    ConstructBody();
+}
+
+Line::~Line() {
+
+}
+
+void Line::Draw(bool simple) {
+    if(simple) { // Just draw a straight line
+        if(m_type >= 5) { // A hidden or base line
+            return; // Don't draw
+        }
+        sf::Vertex simpleLine[] = {sf::Vertex(m_start, sf::Color::Black),
+                                   sf::Vertex(m_finish, sf::Color::Black)};
+        m_window->draw(simpleLine, 2, sf::Lines);
+    } else {
+        m_window->draw(m_body);
+    }
+}
+
+void Line::ConstructBody() {
+    m_body.clear();
+    //m_body.setPrimitiveType(sf::Lines);
+    sf::Color color = sf::Color::Black;
+
+    sf::Vector2f dist = m_finish - m_start;
+    double length = sqrt(dist.x * dist.x + dist.y * dist.y);
+    dist /= (float)length;
+    sf::Vector2f arrow, loc, dot; // For hidden and base lines
+    int numDots = -1;
+
+    if(m_type != lt_base && m_type != lt_hidden) { // No dots
+        m_body.append(sf::Vertex(m_start, color));
+        m_body.append(sf::Vertex(m_finish, color));
+    }
+    switch(m_type) {
+    case lt_topRight:
+        m_body.append(sf::Vertex(m_finish, color));
+        m_body.append(sf::Vertex(m_finish - sf::Vector2f(dist.x, dist.y) * (float)ARROW_LENGTH
+                                          + sf::Vector2f(dist.y*ARROW_LENGTH, dist.x*-ARROW_HEIGHT), color));
+        return;
+    case lt_botRight:
+        m_body.append(sf::Vertex(m_finish, color));
+        m_body.append(sf::Vertex(m_finish - sf::Vector2f(dist.x, dist.y) * (float)ARROW_LENGTH
+                             + sf::Vector2f(dist.y*-ARROW_LENGTH, dist.x*ARROW_HEIGHT), color));
+        return;
+    case lt_topLeft:
+        m_body.append(sf::Vertex(m_start, color));
+        m_body.append(sf::Vertex(m_start + sf::Vector2f(dist.x, dist.y) * (float)ARROW_LENGTH
+                                         + sf::Vector2f(dist.y*ARROW_LENGTH, dist.x*-ARROW_HEIGHT), color));
+        return;
+    case lt_botLeft:
+        m_body.append(sf::Vertex(m_start, color));
+        m_body.append(sf::Vertex(m_start + sf::Vector2f(dist.x, dist.y) * (float)ARROW_LENGTH
+                                         + sf::Vector2f(dist.y*-ARROW_LENGTH, dist.x*ARROW_HEIGHT), color));
+        return;
+    case lt_base:
+        m_body.setPrimitiveType(sf::Points);
+        arrow = m_finish - sf::Vector2f(dist.x, dist.y) * (float)ARROW_LENGTH
+                                  + sf::Vector2f(dist.y*ARROW_LENGTH, dist.x*-ARROW_HEIGHT);
+        loc = m_start;
+        dot = dist * (float)DOT_SIZE;
+        numDots = length / DOT_SIZE / 2;
+        for(int iii = 0; iii < numDots; iii++) { // Draw the dots along the line
+            m_body.append(sf::Vertex(loc, color));
+            m_body.append(sf::Vertex(loc + dot, color));
+            loc += dot * (float)2.0;
+        }
+        m_body.append(sf::Vertex(loc, color));
+        m_body.append(sf::Vertex(m_finish, color));
+
+        loc = m_finish;
+        dot = (arrow - m_finish);
+        dot *= (float)(DOT_SIZE / sqrt(dot.x*dot.x + dot.y*dot.y));
+        numDots = sqrt(ARROW_HEIGHT*ARROW_HEIGHT + ARROW_LENGTH*ARROW_LENGTH) / DOT_SIZE / 2; // TODO: Don't recalculate this every time
+        for(int iii = 0; iii < numDots; iii++) { // Draw the dots along the arrow
+            m_body.append(sf::Vertex(loc, color));
+            m_body.append(sf::Vertex(loc + dot, color));
+            loc += dot * (float)2.0;
+        }
+        m_body.append(sf::Vertex(loc, color));
+        m_body.append(sf::Vertex(arrow, color));
+        break;
+    case lt_hidden:
+        loc = m_start;
+        dot = sf::Vector2f(dist.y, -dist.x) * (float)DOT_DIST;
+        numDots = length / DOT_DIST / 2;
+        for(int iii = 0; iii < numDots; iii++) {
+            m_body.append(sf::Vertex(loc - dot, color));
+            loc += dist * (float)DOT_DIST;
+            m_body.append(sf::Vertex(loc, color));
+            loc += dist * (float)DOT_DIST;
+            m_body.append(sf::Vertex(loc + dot, color));
+        }
+        break;
+    default: // Solid line, no arrow
+       return;
+    }
+}
+
+line_type Line::GetType() {
+    return m_type;
+}
+
+void Line::SetType(line_type newType) {
+    m_type = newType;
+    ConstructBody();
+}
+
+void Line::SetPosition(sf::Vector2f start, sf::Vector2f finish) {
+    m_start = start;
+    m_finish = finish;
+
+    ConstructBody();
+}
---
+++ line.h
@@ -0,0 +1,46 @@
+#ifndef LINE_H
+#define LINE_H
+
+#include <SFML/Graphics.hpp>
+#define ARROW_HEIGHT 7.5
+#define ARROW_LENGTH 15.0
+#define DOT_SIZE 5 // Length of lines in dotted lines
+#define DOT_DIST 1 // Distance between dots in hidden lines
+
+// The first four types refer to the location of the arrow
+// on horizontal line in the positive x direction
+enum line_type {
+    lt_base = 0,    // The base line on the template
+    lt_topRight = 1,
+    lt_botRight = 2,
+    lt_topLeft = 3,
+    lt_botLeft = 4,
+    lt_static = 5, // Not replaced during iterations
+    lt_hidden = 6 // Neither shown nor replaced during iterations
+};
+
+class Line
+{
+private:
+    sf::RenderWindow* m_window;
+    line_type m_type;
+    sf::Vector2f m_start;
+    sf::Vector2f m_finish;
+
+    sf::VertexArray m_body; // The thing that is actually drawn
+
+    void ConstructBody(); // Fills out body based on m_type
+public:
+    Line(sf::RenderWindow* window, line_type type, sf::Vector2f start, sf::Vector2f finish);
+    ~Line();
+
+    void Draw(bool simple);
+    line_type GetType();
+    void SetType(line_type newType);
+    void SetPosition(sf::Vector2f start, sf::Vector2f finish);
+
+};
+
+
+
+#endif // LINE_H
